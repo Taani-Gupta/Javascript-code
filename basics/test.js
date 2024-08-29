07695021dcad51f6ac6032b0018f63e1077c01b4
Taani-Gupta <taani2021happy@gmail.com>
2024-08-29 01:10:50 +0000

    "create a html file"

--- basics/test.js
+++ basics/test.js
@@ -1,12 +1,4 @@
-const accountId=144553
-let accountEmail="taani@google.com"
-var accountPassword="12345"
-accountCity="Jaipur"
-accountEmail="mohini@google.com"
-accountPassword="00000"
-accountCity="Bengaluru"
-let accountState
-console.table([accountEmail, accountId, accountPassword, accountState,accountCity])
+
 
 
 
